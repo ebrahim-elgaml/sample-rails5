7a519a5cd4792ce4c6d8d6ba828d272d13a28534
Ebrahim <ebrahim.el.gaml@gmail.com>
2017-01-05 00:42:29 +0200

    specs for user login and register

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,76 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+  describe "register" do
+    before do
+      @body = {
+                user: {
+                  email: "user.email@test.com",
+                  first_name: "fname",
+                  last_name: "lname",
+                  password: "123456789"
+                }
+             }
+    end
+
+    context "failure" do
+      it "returns email already exists" do
+        user = create :user
+        @body[:user][:email] = user.email
+        post :create, @body, format: :json
+        expect(response.status).to eq(422)
+      end
+
+      it "returns password too short" do
+        @body[:user][:password] = "123"
+        post :create, @body, format: :json
+        expect(response.status).to eq(422)
+      end
+
+      it "returns missing data" do
+        @body[:user][:first_name] = ""
+        post :create, @body, format: :json
+        expect(response.status).to eq(422)
+      end
+    end
+
+    context "succes" do
+      it "works fine" do
+        post :create, @body, format: :json
+        expect(response.status).to eq(201)
+      end
+    end
+  end
+
+  describe "login" do
+    before do
+      user = build :user
+      user.password = "123456789"
+      user.save!
+      @body = {
+                user: {
+                  email: user.email,
+                  password: "123456789"
+                }
+              }
+    end
+
+    it "works fine" do
+      post :login, @body, format: :json
+      expect(response.status).to eq(201)
+      response.body[:api_key].should_not be_blank
+    end
+
+    it "fails as wrong password" do
+      @body[:user][:password] = "sadhkajshdjkdash"
+      post :login, @body, format: :json
+      expect(response.status).to eq(:unauthorized)
+    end
+
+    it "fails as wrong email" do
+      @body[:user][:email] = "n@test.com"
+      post :login, @body, format: :json
+      expect(response.status).to eq(:unauthorized)
+    end
+  end
+end
